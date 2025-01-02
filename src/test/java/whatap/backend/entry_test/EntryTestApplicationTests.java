721ce48be2a4f3f34a15d688462d8eeccd6e86bf
GeoYeo-DreamTree <45192460+Huichan1209@users.noreply.github.com>
2025-01-02 10:11:00 +0900

    개발환경 세팅

---
+++ src/test/java/whatap/backend/entry_test/EntryTestApplicationTests.java
@@ -0,0 +1,13 @@
+package whatap.backend.entry_test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntryTestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
