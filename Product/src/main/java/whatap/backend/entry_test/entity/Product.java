7a6c16d967ace3776a3d50a497fbb80cf922024c
GeoYeo-DreamTree <45192460+Huichan1209@users.noreply.github.com>
2025-01-02 21:00:31 +0900

    Product Entity 구현

---
+++ Product/src/main/java/whatap/backend/entry_test/entity/Product.java
@@ -0,0 +1,43 @@
+package whatap.backend.entry_test.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+public class Product
+{
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private String description;
+
+    private BigDecimal price;
+
+    private Integer stock_quantity;
+
+    @Column(updatable = false)
+    private LocalDateTime created_at;
+
+    private LocalDateTime updated_at;
+
+    @PrePersist
+    private void onCreate()
+    {
+        this.created_at = LocalDateTime.now();
+        this.updated_at = LocalDateTime.now();
+    }
+
+    @PreUpdate
+    private void onUpdate()
+    {
+        this.updated_at = LocalDateTime.now();
+    }
+}
